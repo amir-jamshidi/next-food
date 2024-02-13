4bcdaf7c8a5ce71e1b34c75e607741f05f2ba485
Amir Jamshidi <aabbccddee9898@gmail.com>
2024-02-13 18:34:31 +0330

    Create Dessert Section

--- components/templates/main/CategoriesSection/CategoriesSection.jsx
+++ components/templates/main/CategoriesSection/CategoriesSection.jsx
@@ -1,13 +1,31 @@
 import TitleSection from "@/components/modules/TitleSection/TitleSection";
 import { getCategoires } from "@/libs/requests";
+import Image from "next/image";
 import React from "react";
-
+import imgSam from "@/public/images/pizzasam.png";
 const CategoriesSection = async () => {
   const categories = await getCategoires();
 
   return (
     <div>
-      <TitleSection title={"منوی دسته بندی هــا"} />
+      <TitleSection title={"دسته بندی هــا"} />
+      <div className="grid grid-cols-6 gap-2 my-12 divide-x divide-black/5" dir="ltr">
+        {categories.map((category) => (
+          <div className="" key={category._id}>
+            <div className="relative rounded-full">
+              <Image
+                className=""
+                src={imgSam}
+                style={{ objectFit: "contain" }}
+                alt=""
+              />
+            </div>
+            <div className="flex justify-center">
+              <p className="text-lg text-red-500">{category.title}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- components/templates/main/DessertSection/DessertSection.jsx
+++ components/templates/main/DessertSection/DessertSection.jsx
@@ -1,10 +1,18 @@
+import MealItem from "@/components/modules/MealItem/MealItem";
 import TitleSection from "@/components/modules/TitleSection/TitleSection";
+import { getMeals } from "@/libs/requests";
 import React from "react";
 
-const DessertSection = () => {
+const DessertSection = async () => {
+  const desserts = await getMeals("dessert");
   return (
     <div>
       <TitleSection title={"دسـر"} />
+      <div className="grid grid-cols-4 gap-2 my-12">
+        {desserts.map((desserts) => (
+          <MealItem key={desserts._id} meal={desserts} />
+        ))}
+      </div>
     </div>
   );
 };
