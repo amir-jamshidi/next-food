ed85d6163a33be06d8d95c9720639423109ebe1c
Amir Jamshidi <aabbccddee9898@gmail.com>
2024-02-13 21:56:24 +0330

    Create Beverage Section

--- components/templates/main/BeverageSection/BeverageSection.jsx
+++ components/templates/main/BeverageSection/BeverageSection.jsx
@@ -1,10 +1,19 @@
+import MealItem from "@/components/modules/MealItem/MealItem";
 import TitleSection from "@/components/modules/TitleSection/TitleSection";
+import { getMeals } from "@/libs/requests";
 import React from "react";
 
-const BeverageSection = () => {
+const BeverageSection = async () => {
+  const beverages = await getMeals("beverage");
+  
   return (
     <div>
       <TitleSection title={"نوشیدنی"} />
+      <div className="grid grid-cols-4 gap-2 my-12">
+        {beverages.map((beverage) => (
+          <MealItem key={beverage._id} meal={beverage} />
+        ))}
+      </div>
     </div>
   );
 };
