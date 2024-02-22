199c2adcb3d5efd25e8a83667440e38dfd8dc1e6
Amir Jamshidi <aabbccddee9898@gmail.com>
2024-02-22 22:00:26 +0330

    Fix Login UI

--- components/templates/login/LoginSubmitForm/LoginSubmitForm.jsx
+++ components/templates/login/LoginSubmitForm/LoginSubmitForm.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+
 import { useFormStatus } from "react-dom";
 const LoginSubmitForm = ({ value }) => {
   const { pending } = useFormStatus();
@@ -10,7 +11,7 @@ const LoginSubmitForm = ({ value }) => {
       className={`${
         pending ? "bg-gray-300" : "bg-green-500 "
       } w-full rounded-2xl text-gray-100 py-2.5 text-base transition-colors`}
-    >{`${pending ? "صبر کنید ..." : value}`}</button>
+    >{`${pending ? "صبر کنید ..." : value}`}</button> 
   );
 };
 
